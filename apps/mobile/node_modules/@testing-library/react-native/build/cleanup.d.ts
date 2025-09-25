5c038672de17404e79a8f87f55efd2029b72e48f
yalka2024 <yanp07@gmail.com>
2025-09-24 21:32:07 -0700

    Remove node_modules and update .gitignore to exclude large files

--- apps/mobile/node_modules/@testing-library/react-native/build/cleanup.d.ts
+++
@@ -1,6 +0,0 @@
-type CleanUpFunction = () => void;
-type CleanUpFunctionAsync = () => Promise<void>;
-export default function cleanup(): void;
-export declare function cleanupAsync(): Promise<void>;
-export declare function addToCleanupQueue(fn: CleanUpFunction | CleanUpFunctionAsync): void;
-export {};
--- apps/mobile/node_modules/@testing-library/react-native/build/render-act.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ReactTestRenderer, TestRendererOptions } from 'react-test-renderer';
-export declare function renderWithAct(component: React.ReactElement, options?: Partial<TestRendererOptions>): ReactTestRenderer;
-export declare function renderWithAsyncAct(component: React.ReactElement, options?: Partial<TestRendererOptions>): Promise<ReactTestRenderer>;
